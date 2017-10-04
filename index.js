eed1e58656bda78e5255559081bd5dea66e67dd0
JohnMichelin <jmichelinphone@gmail.com>
2017-10-04 10:06:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var Twit = require("twit");
+require("dotenv").config();
+
+var bot = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
+});
+
+// @ahandvanish
+
+/*
+Send Tweet
+
+bot.post(
+  "statuses/update",
+  { status: "Thank you so much @ahandvanish for helping #twit.js this tweet!" },
+  function(err, data, response) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${data.text} was tweeted`);
+    }
+  }
+);
+/*
+get a list of user id that follow a screenname
+*/
+bot.get(
+  "followers/ids",
+  {
+    screenname: "Journey4John",
+    cursor: -1,
+    count: 20
+  },
+  function(err, data, response) {
+    if (err) {
+      console.error(err);
+    } else {
+      //console.log(data.ids);
+      //console.log(data.next_cursor);
+    }
+  }
+);
+
+// bot.get("followers/list", { screenname: "Journey4John" }, function(err, data, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(data.users.length);
+//     data.users.forEach(function(user) {
+//       //console.log(user.screen_name)
+//     });
+//     //console.log(response)
+//   }
+// });
+
+bot.get("statuses/home_timeline", { count: 5 }, function(err, data, response) {
+  if (err) {
+    console.error('Error\n', err);
+  } else {
+    console.log(data);
+    //console.log(data.next_cursor);
+  }
+});
