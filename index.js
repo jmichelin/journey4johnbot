6477813441b24e3f76746685ac0fd85d5a36c3ed
JohnMichelin <jmichelinphone@gmail.com>
2017-10-05 16:14:00 -0400

    feat(streams): add stream info

--- index.js
+++ index.js
@@ -28,22 +28,22 @@ bot.post(
 /*
 get a list of user id that follow a screenname
 */
-bot.get(
-  "followers/ids",
-  {
-    screenname: "Journey4John",
-    cursor: -1,
-    count: 20
-  },
-  function(err, data, response) {
-    if (err) {
-      console.error(err);
-    } else {
-      //console.log(data.ids);
-      //console.log(data.next_cursor);
-    }
-  }
-);
+// bot.get(
+//   "followers/ids",
+//   {
+//     screenname: "Journey4John",
+//     cursor: -1,
+//     count: 20
+//   },
+//   function(err, data, response) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       //console.log(data.ids);
+//       //console.log(data.next_cursor);
+//     }
+//   }
+// );
 
 // bot.get("followers/list", { screenname: "Journey4John" }, function(err, data, response) {
 //   if (err) {
@@ -74,20 +74,140 @@ function getBotTimeline() {
   });
 }
 
-getBotTimeline();
+//getBotTimeline();
 
-bot.post("statuses/retweet/:id", { id: '' }, function(err, data, response) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`${data.text} was retweeted`);
-  }
-});
+// bot.post("statuses/retweet/:id", { id: '' }, function(err, data, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(`${data.text} was retweeted`);
+//   }
+// });
+//
+// bot.post("statuses/unretweet/:id", { id: '' }, function(err, data, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(`${data.text} was retweeted`);
+//   }
+// });
 
-bot.post("statuses/unretweet/:id", { id: '' }, function(err, data, response) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`${data.text} was retweeted`);
-  }
+// bot.post("favorites/create", { id: "915661607648116736" }, function(err, data, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(`${data.text} was favorited`);
+//   }
+// });
+//
+// bot.post("favorites/destroy", { id: "915661607648116736" }, function(err, data, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(`${data.text} was unfavorited`);
+//   }
+// });
+
+//reply to a tweet
+// bot.post(
+//   "statuses/update",
+//   {
+//     status: "@Space_Station Now we can go to #mars!!",
+//     in_reply_to_status_id: "915990634103820290"
+//   },
+//   function(err, data, response) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log(`${data.text} was retweeted`);
+//     }
+//   }
+// );
+
+//remove reply
+// bot.post(
+//   "statuses/destroy",
+//   {
+//     status: "@Space_Station Now we can go to #mars!!",
+//     in_reply_to_status_id: "915990634103820290"
+//   },
+//   function(err, data, response) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log(`${data.text} was retweeted`);
+//     }
+//   }
+// );
+// twbg twbp
+
+/*
+
+Search types
+q: 'single'
+q: 'both words'
+q: '"exact match"'
+q: 'this OR that'
+q: 'this -notThat -orNotTheOther'
+q: ':( emoticons'
+q: '#hashtags'
+q: 'to@journey4John'
+q: 'from@journey4John'
+q: 'dance filter:safe' //g rated
+q: 'dance filter:media' //videos and pictures
+q: 'dance filter:vine' //
+q: 'dance filter:images'
+q: 'dance filter:links'
+q: 'dance url:amazon'
+q: 'dance ?' //search for questions
+q: 'dance since:01/11/2017'
+
+additional config params
+
+result_type: recent/popular
+geocode: 'lat, lng, radius' // '40.749175, -73.977369, 1mi'
+lang: 'es'
+
+
+
+
+ */
+
+// bot.get(
+//   "search/tweets",
+//   {
+//     q: '"corgi butt"',
+//     count: 5
+//   },
+//   function(err, data, response) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       // console.log(data);
+//       data.statuses.forEach(s => {
+//         console.log(s.text);
+//         console.log(s.user.screen_name);
+//         console.log("\n");
+//       });
+//     }
+//   }
+// );
+
+// random sampling of live tweets
+// var stream = bot.stream('statuses/sample');
+
+// stream.on('tweet', function(tweet){
+//   console.log(tweet.text+'\n');
+// });
+//
+
+//var stream = bot.stream('statuses/filter', { track: 'corgi, dog' }); //or
+//var stream = bot.stream('statuses/filter', { track: 'corgi dog' }); //and
+//space = logical and , logical or
+// var stream = bot.stream('statuses/filter', { track: 'corgi' });
+
+var stream = bot.stream('statuses/filter', { locations: '-74,40,-73,41'})
+
+stream.on('tweet', function(tweet){
+  console.log(tweet.text+'\n');
 });
