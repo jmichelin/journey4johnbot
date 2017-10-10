6729b829a7a4c83a70ccc82c324f38d1fe07f3ac
JohnMichelin <jmichelinphone@gmail.com>
2017-10-10 14:01:09 -0400

    feat(google vision): add google vision to project

--- index.js
+++ index.js
@@ -1,61 +1,13 @@
 var Twit = require("twit");
 var fs = require("fs");
-var csvParse = require("csv-parse");
-var rita = require('rita');
-var inputText = '';
+var request = require('request');
+var vision = require('@google-cloud/vision')({
+  projectId: 'johnbot',
+  keyFilename: './keyfile.json'
+})
 
 require("dotenv").config();
 
-var filePath = './private/twitter_archive/tweets.csv';
-
-var tweetData = fs.createReadStream(filePath)
-  .pipe(csvParse({delimiter: ','}))
-  .on('data', function(row){
-    //console.log(row[5]);
-    inputText = `${inputText} ${cleanText(row[5])}`;
-  })
-  .on('end', function(){
-    var markov = new rita.RiMarkov(4);
-    markov.loadText(inputText);
-    var sentence = markov.generateSentences(2);
-    console.log(sentence);
-    // bot.post(
-    //   "statuses/update",
-    //   {
-    //     status: sentence
-    //   },
-    //   function (err, data, response) {
-    //     if (err) {
-    //       console.error(err);
-    //     } else {
-    //       console.log(`${data}`);
-    //     }
-    //   }
-    // );
-  });
-
-
-function hasNoStopwords(token){
-  var stopwords = ['@', 'http', 'RT', '#', ':', '_'];
-  return stopwords.every(function(sw){
-    return !token.includes(sw)
-  })
-}
-
-function cleanText(text) {
-  return rita.RiTa.tokenize(text, ' ')
-    .filter(hasNoStopwords)
-    .join(' ')
-    .trim();
-}
-
-// var markov = new rita.RiMarkov(3);
-// markov.loadText(inputText);
-// var sentences = markov.generateSentences(1);
-//
-// console.log(sentences);
-// console.log(markov.getProbability('went'));
-// console.log(markov.getProbabilities('went'));
 
 var bot = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -64,69 +16,3 @@ var bot = new Twit({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000
 });
-
-
-
-// function getNasaPhoto() {
-//   var parameters = {
-//     url: "https://api.nasa.gov/planetary/apod",
-//     qs: {
-//       api_key: process.env.NASA_API_KEY
-//     },
-//     encoding: "binary"
-//   };
-//   request.get(parameters, function(err, response, body) {
-//     body = JSON.parse(body);
-//     saveFile(body, 'nasa.jpg');
-//     // saveFile("https://media3.giphy.com/media/gixQfE7XzZfpe/giphy.gif", "nasa.gif");
-//   });
-// }
-//
-// function saveFile(body, fileName) {
-//   var file = fs.createWriteStream(fileName);
-//   request(body).pipe(file).on("close", function(err) {
-//     if (err) {
-//       console.error("Error", err);
-//     } else {
-//       console.log("Media Saved");
-//       var descriptionText = body.title
-//       //var descriptionText = 'Dancing Corgi Time'
-//       uploadMedia(descriptionText, fileName);
-//     }
-//   });
-// }
-//
-// function uploadMedia(descriptionText, fileName) {
-//
-//   var filePath = __dirname + "/" + fileName;
-//   bot.postMediaChunked({ file_path: filePath }, function(err, data, response) {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       console.log(data);
-//       var params = {
-//         status: descriptionText,
-//         media_ids: data.media_id_string
-//       };
-//       postStatus(params);
-//     }
-//   });
-// }
-//
-//
-// function postStatus(params){
-//   bot.post(
-//     "statuses/update",
-//     params,
-//     function (err, data, response) {
-//       if (err) {
-//         console.error(err);
-//       } else {
-//         console.log(`Status Posted`);
-//       }
-//     }
-//   );
-// }
-
-
-// getNasaPhoto();
