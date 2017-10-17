6d748de049a9c157eb92e959b61bd3ca07a4e1fa
JohnMichelin <jmichelinphone@gmail.com>
2017-10-17 13:19:29 -0400

    feat(tracery): tell a simple star wars story in a sentence

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var Twit = require("twit");
-var fs = require("fs");
-var request = require("request");
-var vision = require("@google-cloud/vision")({
-  projectId: "johnbot-182518",
-  keyFilename: "./private/keyfile.json"
-});
+var tracery = require('tracery-grammar');
+
 require("dotenv").config();
 
 var bot = new Twit({
@@ -15,102 +11,18 @@ var bot = new Twit({
   timeout_ms: 60 * 1000
 });
 
-function downloadPhoto(url, replyToName, tweetId) {
-  var paramaters = {
-    url: url,
-    encoding: "binary"
-  };
-  request.get(paramaters, function(err, response, body) {
-    var filename = "photo" + Date.now() + ".jpg";
-    fs.writeFile(filename, body, "binary", function(err) {
-      console.log("Downloaded Photo.");
-      analyzePhoto(filename, replyToName, tweetId);
-    });
-  });
-}
-
-function analyzePhoto(filename, replyToName, tweetId) {
-  var request = {source: { filename: filename}};
-  vision.faceDetection(request, function(err, faces) {
-    console.log("faces.faceAnnotations:", faces.faceAnnotations);
-    var allEmotions = [];
-    faces.faceAnnotations.forEach(function(face){
-      extractFaceEmotions(face).forEach(function(emotion){
-        if(allEmotions.indexOf(emotion) === -1){
-          allEmotions.push(emotion);
-        }
-      });
-    });
-    postStatus(allEmotions, replyToName, tweetId);
-  });
-}
-
-function extractFaceEmotions(face){
-  var emotions = ['joyLikelihood', 'sorrowLikelihood', 'angerLikelihood', 'surpriseLikelihood'];
-  return emotions.filter(function(emotion){
-    console.log(face[emotion]);
-    return face[emotion] === 'LIKELY' || face[emotion] === 'VERY_LIKELY';
-  })
-}
-
-function postStatus(allEmotions, replyToName, tweetId){
-  var status = formatStatus(allEmotions, replyToName)
-  bot.post(
-    "statuses/update",
-    {
-      status: status,
-      in_reply_to_status_id: tweetId
-    },
-    function (err, data, response) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Bot has tweeted ${status}`);
-      }
-    }
-  );
-}
-
-function formatStatus(allEmotions, replyToName){
-  var reformatEmotions = {
-    joyLikelihood: 'happy',
-    sorrowLikelihood: 'sad',
-    angerLikelihood: 'angry',
-    surpriseLikelihood: 'surprised',
-  };
-  var status = `@${replyToName} looking`;
-  if (allEmotions.length>0){
-    allEmotions.forEach(function(emotion, i){
-      if(i === 0 ){
-        status = `${status} ${reformatEmotions[emotion]}`
-      } else {
-        status = `${status} and ${reformatEmotions[emotion]}`
-      }
-    });
-  } else {
-    status = `${status} neutral`
-  }
-  status = `${status}!`
-  return status
-}
-
-var stream = bot.stream("statuses/filter", { track: "@journey4john" });
-
-stream.on("connecting", function(response) {
-  console.log("Connecting....");
+var grammar = tracery.createGrammar({
+  'character': ['Finis Valorum', 'Bail Prestor Organa', 'Jango Fett', 'Sebulba', 'Adi Gallia'],
+  'action': ['force push', 'jetpack', 'charge', 'blast'],
+  'place': ['Tund', 'the Scimitar', 'Jakku', 'the flight deck', 'Tashi Station', 'Slave 1'],
+  'object': ['DL-44', 'protocol droid', 'light saber'],
+  'origin': ['#character.capitalize# #action.ed# #character.capitalize# on #place# to get some #object.s#.']
 });
+// #object.a# adds a or an
+// #object.s# pluralizes
 
-stream.on("connected", function(response) {
-  console.log("Connected");
-});
+grammar.addModifiers(tracery.baseEngModifiers);
 
-stream.on("error", function(err) {
-  console.log(err);
-});
+var story = grammar.flatten('#origin#');
 
-stream.on("tweet", function(tweet) {
-  console.log("Checking Tweet.");
-  if (tweet.entities.media) {
-    downloadPhoto(tweet.entities.media[0].media_url, tweet.user.screen_name, tweet.id_str);
-  }
-});
+console.log(story);
